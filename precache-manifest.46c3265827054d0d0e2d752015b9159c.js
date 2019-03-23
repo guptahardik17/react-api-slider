3cea1bb1dc50753dd1696a4d74589ff70e642400
guptahardik17 <guptahardik17@gmail.com>
2019-03-23 08:25:26 +0530

    Updates

---
+++ precache-manifest.46c3265827054d0d0e2d752015b9159c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e033a8445ece4ea45ff8",
+    "url": "/react-api-slider/static/css/main.0ab62581.chunk.css"
+  },
+  {
+    "revision": "e033a8445ece4ea45ff8",
+    "url": "/react-api-slider/static/js/main.3fe5123a.chunk.js"
+  },
+  {
+    "revision": "ed11a4133a649c33485c",
+    "url": "/react-api-slider/static/js/runtime~main.f4beb8fa.js"
+  },
+  {
+    "revision": "234caa4b812d702d8cfa",
+    "url": "/react-api-slider/static/js/2.5d6749da.chunk.js"
+  },
+  {
+    "revision": "93714d882884f0d08bc43b008ce72d16",
+    "url": "/react-api-slider/index.html"
+  }
+];
